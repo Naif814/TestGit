2f07603ca55bcfcfd718f55083c696703073839b
Mohammed Dawood Ali <mdawood@lotlinx.com>
2020-05-27 21:56:28 -0400

    Pushed

---
+++ two/src/main/java/org/test/two/two/App.java
@@ -0,0 +1,13 @@
+package org.test.two.two;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ two/src/main/java/org/test/two/two/TestTwo.java
@@ -0,0 +1,13 @@
+package org.test.two.two;
+
+public class TestTwo {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hello World 123!");
+		System.out.println("Hello World 1234567!");
+		
+	}
+
+}
